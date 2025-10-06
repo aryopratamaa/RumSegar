c719df9c17a25aef0feffbd8868af0e268b88414
aryopratamaa <mr.aryopratama@gmail.com>
2025-10-06 17:21:51 +0700

    17.21

--- app/src/main/java/com/goyo/rumsegar/ProfilActivity.kt
+++ app/src/main/java/com/goyo/rumsegar/ProfilActivity.kt
@@ -1,7 +1,9 @@
 package com.goyo.rumsegar
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.LinearLayout
@@ -17,6 +19,29 @@ class ProfilActivity : AppCompatActivity() {
         val btnHome = findViewById<LinearLayout>(R.id.btnHome)
         btnHome.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnWhatsapp = findViewById<ImageView>(R.id.btnWhatsapp)
+        btnWhatsapp.setOnClickListener {
+            val noHp ="6282161965317"
+            val pesan = "Halo, saya ingin bertanya"
+            val url = "https://api.whatsapp.com/send?phone=$noHp&text=$pesan"
+            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(url))
+            startActivity(intent)
+        }
+
+        val btnInstagram = findViewById<ImageView>(R.id.btnInstagram)
+        btnInstagram.setOnClickListener {
+            val url = "https://www.instagram.com/aryopratamaa"
+            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(url))
+            startActivity(intent)
+        }
+
+        val btnFacebook = findViewById<ImageView>(R.id.btnFacebook)
+        btnFacebook.setOnClickListener {
+            val url = "https://www.facebook.com/aryopratama04082000"
+            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(url))
             startActivity(intent)
         }
 
